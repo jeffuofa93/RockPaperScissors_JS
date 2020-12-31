36a1c775820657c0e587174c408c0026e5d6a80d
jeffuofa93 <wiederkehr@email.arizona.edu>
2020-12-31 07:59:28 -0700

    -all

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -11,26 +11,71 @@ function computerPlay(){
 }
 
 
-function playRound(playerSelection,computerSelection,counter=0){
+function playRound(playerSelection,score){
+    // it's running
+    computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
-        nextMove = computerPlay();
-        playerselection = prompt("It's a tie enter rock paper or scissors to continue this round: ");
-        return playRound(playerSelection,nextMove,counter++);
+        alert("Tie");
+        // come back to ties
+        //nextMove = computerPlay();
+        //playerselection = prompt("It's a tie enter rock paper or scissors to continue this round: ");
+        //return playRound(playerSelection,nextMove,counter++);
     } else if ((computerSelection === "rock" && playerSelection ==="scissors")||
         (computerSelection ==="paper" && playerSelection ==="rock")||
         (computerSelection==="scissors" && playerSelection ==="paper")){
             //console.log("You Lose! " + computerSelection+ " beats " + playerSelection);
             //console.log("end of inner function \n");
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
+            score[1] ++;
+            userScore.textContent = "Your Score: " + score[0];
+            computerScore.textContent = "Your Score: " + score[1];
+            content.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`
     }
     else {
         //console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
         //console.log("end of inner function \n");
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        score[0] ++;
+        userScore.textContent = "Your Score: " + score[0];
+        computerScore.textContent = "Your Score: " + score[1];
+        content.textContent =  `You Win! ${playerSelection} beats ${computerSelection}`
+        //return `You Win! ${playerSelection} beats ${computerSelection}`
     }
     
 }
 
+
+
+// Add div to display results of each round
+// user is first index computer is second
+let score = [0,0];
+const result = document.querySelector("#Result");
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "You lose";
+result.appendChild(content);
+
+const userScore = document.querySelector("#UserScore");
+const computerScore = document.querySelector("#ComputerScore");
+
+
+const rock = document.querySelector("#Rock");
+let rockID = rock.id.toLowerCase();
+//rock.addEventListener("click",playRound(rockID,score));
+
+rock.addEventListener('click',playRound(rockID,score));
+
+//score = rock.addEventListener("click",playRound())
+
+// function to go through the button clicks
+
+
+
+
+
+
+
+
+
+/*
 function game(){
     let displayString;
 
@@ -40,10 +85,29 @@ function game(){
         displayString = playRound(playerSelection,computerSelection);
         console.log(displayString);
     }
-}
+} */
 
-game();
+/*
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-// 
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+ */
+
+
 
 // EOF comment
